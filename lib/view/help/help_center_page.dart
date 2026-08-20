4d572a01efe78b51c756221c8e2268d1ca0b0166
自在清风 <xuehui.yang888@icloud.com>
2026-08-21 01:56:26 +0800

    更新到1.0.9

--- lib/view/help/help_center_page.dart
+++ lib/view/help/help_center_page.dart
@@ -34,7 +34,7 @@ final AppHelpCenterConfig gongKeHelpConfig = AppHelpCenterConfig(
   ],
   versionHistory: [
      VersionHistoryItem(
-      versionName: 'v1.0.8',
+      versionName: 'v1.0.9',
       publishedAt: DateTime(2026, 8, 21),
       changes: '1.完善开示功能\n2.完善电子木鱼,增加十念法\n3.完善听书功能',
     ),
